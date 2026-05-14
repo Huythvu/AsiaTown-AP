e99c1c215f9194ecc1b4237dedda6a702c849fe7
Huy Vu <10643808@ucn.dk>
2026-05-14 19:48:08 +0200

    delete Plugin csv import-export

--- strapi/config/plugins.js
+++ strapi/config/plugins.js
@@ -1,15 +1,1 @@
-module.exports = () => ({
-  "strapi-csv-import-export": {
-    enabled: true,
-    config: {
-      authorizedExports: [
-        "api::product.product",
-        "api::recipe.recipe"
-      ],
-      authorizedImports: [
-        "api::product.product",
-        "api::recipe.recipe"
-      ]
-    }
-  }
-});
+module.exports = () => ({});
