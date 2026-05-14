36ffc38a6ecc330afeeb83e04563916cf166cc83
Huy Vu <10643808@ucn.dk>
2026-05-14 17:46:16 +0200

    add strapi plugin

--- strapi/config/plugins.js
+++ strapi/config/plugins.js
@@ -1,1 +1,15 @@
-module.exports = () => ({});
+module.exports = () => ({
+  "strapi-csv-import-export": {
+    enabled: true,
+    config: {
+      authorizedExports: [
+        "api::product.product",
+        "api::recipe.recipe"
+      ],
+      authorizedImports: [
+        "api::product.product",
+        "api::recipe.recipe"
+      ]
+    }
+  }
+});
